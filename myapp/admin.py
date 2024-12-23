58db9c51e10823a6f84ae55fc003a24a74b95d74
Fauzia599 <fauzali98@gmail.com>
2024-12-23 02:22:27 -0800

    my third

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-# Register your models hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.
+# Register your models 
 
 admin.site.register(Task)
 admin.site.register(Cleaner)
